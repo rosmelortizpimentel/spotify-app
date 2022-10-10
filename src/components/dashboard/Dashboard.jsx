864ac1b19cdb16ef4c2d4894c41fb14386d32ecc
Rosmel Ortiz <rosmelortizpimentel@gmail.com>
2022-10-09 23:39:43 -0500

    Subiendo compilado

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -1,8 +1,4 @@
 import { Typography } from '@mui/material';
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
 import { useParams } from 'react-router-dom';
 
 export const Dashboard = () => {
